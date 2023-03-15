9f9cb3774fe210e94fc37b3babf009d2379a9e35
RosaD97 <r960595@gmail.com>
2023-03-15 16:57:17 +0100

    milestone 1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-alert('funziona')
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            title: 'VUE TO DO LIST',
+            list: [
+                {
+                    text: 'Fare la spesa',
+                    done: false,
+
+                },
+                {
+                    text: 'Comprare il latte',
+                    done: false,
+
+                },
+                {
+                    text: 'Andare al parco',
+                    done: false,
+
+                }
+            ]
+        }
+    },
+    methods: {
+
+    }
+}).mount('#app');
